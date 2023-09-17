82779b7bdc2e8ade5f08e621c127dc8da370fd0a
hady awny <hadyawny5@gmail.com>
2023-09-17 13:40:09 +0300

    finished design radio tab

--- lib/tabs/radio_tab.dart
+++ lib/tabs/radio_tab.dart
@@ -5,8 +5,24 @@ class RadioTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/radio_body.png"),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            "إذاعة القرآن الكريم",
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Image.asset("assets/images/play_icons_radio.png"),
+        ],
+      ),
     );
   }
 }
